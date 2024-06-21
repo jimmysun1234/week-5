6557cc526296c0e1249ce24f494a281863720cac
孫詠鈞 <jimmysun609@gmail.com>
2024-06-22 03:18:45 +0800

    Update tool.c

--- tool.c
+++ tool.c
@@ -3,15 +3,6 @@
 #include <time.h>
 #include "myhead.h"
 
-void printStudents(Student students[], int numStudents) {
-    // 打印學生信息
-    printf("%-15s %-12s %8s    %-10s\n", "Name", "Department", "Grade", "SID");
-    printf("------------------------------------------------\n");
-    for (int i = 0; i < numStudents; i++) {
-        printf("%-15s %-12s %8d    %-10s\n", students[i].name, students[i].department, students[i].grade, students[i].sid);
-    }
-}
-
 int readSRecordsFromCSV(const char* filename, SRecord srecords[], int maxNumSRecords) {
     // 從CSV文件中讀取學生成績信息
     FILE* file = fopen(filename, "r"); // r=read, w=write, a=append
