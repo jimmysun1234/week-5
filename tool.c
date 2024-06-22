73706ef000d404b85877547de3d350f5dfec2bc2
孫詠鈞 <jimmysun609@gmail.com>
2024-06-23 00:58:01 +0800

    Update tool.c

--- tool.c
+++ tool.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
 #include "myhead.h"
 
-int readSRecordsFromCSV(const char* filename, SRecord srecords[], int maxNumSRecords) {
-    // 從CSV文件中讀取學生成績信息
-    FILE* file = fopen(filename, "r"); // r=read, w=write, a=append
-    if (file == NULL) {
+void printSRecords(SRecord srecords[], int numSRecords) {
+    saveDefaultColor();
+    setColor(GREEN);
+    setBackgroundColor(WHITE);
+    printf("%-8s %5s %8s    %-6s %5s %6s %5s %6s %6s %6s\n", " Name", "Department", "Grade", "SID", "HW", "Quiz", "Mid", "Final", "Other", "Total");
+    printf("--------------------------------------------------------------------------------\n");
+    setColor(BLUE);
+    setBackgroundColor(WHITE);
+    for (int i=0; i<numSRecords; i++){
+        printf("%-14s %-7s %8d    %-10s %4.1f %5.1f %5.1f %5.1f %6.1f %6.1f\n", srecords[i].student.name, srecords[i].student. department, srecords[i].student.grade, srecords[i].student.sid, srecords[i].hw, srecords[i].quiz, srecords[i].midterm, srecords[i].final, srecords[i].other, srecords[i].total);
+    }
+    resetColor();
+}
+int readSRecordsFromCSV(const char* filename, SRecord srecords[], int maxNumSRecodes) {
+    FILE* file=fopen(filename, "r"); //read file
+    if(file==NULL){
         printf("Failed to open file %s\n", filename);
         return 0;
     }
-    // skip header line
     char line[1024];
-    fgets(line, 1024, file);
-    int numSRecords = 0;
-    while (fgets(line, 1024, file) && numSRecords < maxNumSRecords) {
-        sscanf(line, "%[^,],%[^,],%d,%[^,],%lf,%lf,%lf,%lf,%lf", srecords[numSRecords].student.department, srecords[numSRecords].student.sid, &srecords[numSRecords].student.grade, srecords[numSRecords].student.name, &srecords[numSRecords].hw, &srecords[numSRecords].quiz, &srecords[numSRecords].midterm, &srecords[numSRecords].final, &srecords[numSRecords].other);
+    fgets(line, 1024, file); //skip the first line
+    int numSRecords=0;
+    while (fgets(line, 1024, file) && numSRecords< maxNumSRecodes){
+        sscanf(line, "%[^,],%[^,],%d,%[^,],%lf,%lf,%lf,%lf,%lf,%lf", srecords[numSRecords].student.department, srecords[numSRecords].student.sid, &srecords[numSRecords].student.grade, srecords[numSRecords].student.name, &srecords[numSRecords].hw, &srecords[numSRecords].quiz, &srecords[numSRecords].midterm, &srecords[numSRecords].final, &srecords[numSRecords].other);
 
         srecords[numSRecords].total = srecords[numSRecords].hw * HW_WEIGHT + srecords[numSRecords].quiz * QUIZ_WEIGHT + srecords[numSRecords].midterm * MIDTERM_WEIGHT + srecords[numSRecords].final * FINAL_WEIGHT + srecords[numSRecords].other * OTHER_WEIGHT;
-
+        
         numSRecords++;
     }
     fclose(file);
     return numSRecords;
 }
 
-void printSRecords(SRecord srecords[], int numSRecords) {
-    printf("%-8s %5s %8s    %-6s %5s %6s %5s %6s %6s %6s\n", " Name", "Department", "Grade", "SID", "HW", "Quiz", "Mid", "Final", "Other", "Total");
-    printf("--------------------------------------------------------------------------------\n");
-    for (int i=0; i<numSRecords; i++){
-        printf("%-14s %-7s %8d    %-10s %4.1f %5.1f %5.1f %5.1f %6.1f %6.1f\n", srecords[i].student.name, srecords[i].student. department, srecords[i].student.grade, srecords[i].student.sid, srecords[i].hw, srecords[i].quiz, srecords[i].midterm, srecords[i].final, srecords[i].other, srecords[i].total);
-    }
-}
+
